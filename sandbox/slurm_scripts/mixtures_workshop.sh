2fe799db4b51a0db82fbacb9a0d690000db26478
blind-contours <david_mccoy@berkeley.edu>
2023-02-23 11:49:33 -0800

    batch

---
+++ sandbox/slurm_scripts/mixtures_workshop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=prime_mixtures_nhanes
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+
+#SBATCH --qos=biostat_savio2_normal
+#SBATCH --account=co_biostat
+
+# Number of nodes for use case:
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --exclusive
+#
+# Wall clock limit:
+#SBATCH --time 72:00:00
+#
+## Command(s) to run (example):
+module load r/4.0.3
+
+### Run Simulation
+R CMD BATCH --no-save ../NHANES/code/NIEHS_2019_workshop.R mixutres_workshop.Rout

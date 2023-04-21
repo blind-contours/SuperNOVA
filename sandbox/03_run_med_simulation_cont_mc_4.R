d60679b8f92d78e5d8bff32c2b4c6d5a14c9812e
blind-contours <david_mccoy@berkeley.edu>
2023-04-21 14:09:23 -0700

    Scripts for continuous simulations

--- sandbox/03_run_med_simulation_cont_mc_4.R
+++ sandbox/03_run_med_simulation_cont_mc_4.R
@@ -87,5 +87,5 @@ for (sample_size in n_obs) {
 # save results to file
 saveRDS(
   object = sim_results_df,
-  file = here("sandbox/data", paste0("SuperNOVA_", "mediation_cont_mc_1", ".rds"))
+  file = here("sandbox/data", paste0("SuperNOVA_", "mediation_cont_mc_4", ".rds"))
 )

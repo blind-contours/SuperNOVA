251b0238d5c66ef1ee2d508caf7389ae4c2cf264
blind-contours <david_mccoy@berkeley.edu>
2023-05-04 17:20:36 -0700

    Prepare slurm files for mediation simulation.

--- sandbox/03_run_med_simulation_quant_1.R
+++ sandbox/03_run_med_simulation_quant_1.R
@@ -89,5 +89,5 @@ for (sample_size in n_obs) {
 # save results to file
 saveRDS(
   object = sim_results_df,
-  file = here("sandbox/data", paste0("SuperNOVA_", "mediation_quant_hbin_1", ".rds"))
+  file = here("sandbox/data", paste0("SuperNOVA_", "mediation_quant_1", ".rds"))
 )
--- sandbox/03_run_med_simulation_quant_4.R
+++ sandbox/03_run_med_simulation_quant_4.R
@@ -87,5 +87,5 @@ for (sample_size in n_obs) {
 # save results to file
 saveRDS(
   object = sim_results_df,
-  file = here("sandbox/data", paste0("SuperNOVA_", "mediation_quant_9", ".rds"))
+  file = here("sandbox/data", paste0("SuperNOVA_", "mediation_quant_4", ".rds"))
 )

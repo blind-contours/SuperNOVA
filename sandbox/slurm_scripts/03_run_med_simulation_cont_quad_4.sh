e017258a850dd74457921a4b6682cc406291e133
blind-contours <david_mccoy@berkeley.edu>
2023-05-22 18:09:22 -0700

    new shell scripts for adaptive quad

---
+++ sandbox/slurm_scripts/03_run_med_simulation_cont_quad_4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=4_c_quad_mediation_supernova
+#
+# Partition:
+#SBATCH --partition=savio3
+#
+
+#SBATCH --qos=biostat_savio3_normal
+#SBATCH --account=co_biostat
+
+# Number of nodes for use case:
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --exclusive
+#
+# Wall clock limit:
+#SBATCH --time 72:00:00
+#
+## Command(s) to run (example):
+module load r/4.0.3
+
+### Run Simulation
+R CMD BATCH --no-save ../03_run_med_simulation_cont_quad_4.R supernova_mediation_cont_quad_4.Rout
